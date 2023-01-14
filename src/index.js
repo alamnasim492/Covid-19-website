dcd445c960a92e34d1f8ce6c0c2808734de5c101
Nasim <alamnasim492@gmail.com>
2023-01-14 23:21:45 +0530

    add my covid 19 website

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import  ReactDOM from "react-dom"
+import CovidData from "./Component/CovidData"
+ReactDOM.render(
+  <>
+<CovidData/>
+  </>,
+
+document.getElementById("root"))
